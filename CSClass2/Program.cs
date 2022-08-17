3ab0b170083c3273a1338a21697398a6fbd806d0
soooomm <soooomm00@gmail.com>
2022-08-17 10:06:26 +0900

    #2 인덱서

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -18,6 +18,10 @@ namespace CSClass2
             Console.WriteLine(wantedString.Value);
             Console.WriteLine(wantedInt.Value);
             Console.WriteLine(wantedDouble.Value);
+
+            Products p = new Products();
+            Console.WriteLine(p[4]);
+
         }
     }
 }
