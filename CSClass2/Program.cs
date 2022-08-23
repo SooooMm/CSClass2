c36293f1a9926b0db13a53298be95a5ad7921c30
soooomm <soooomm00@gmail.com>
2022-08-24 08:56:05 +0900

    #3 out 키워드 여러개를 반환해서 사용하고 싶을때 사용한다

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -8,8 +8,15 @@ namespace CSClass2
 {
     internal class Program
     {
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
+        }
+
         static void Main(string[] args)
         {
+            /*
             Wanted<string> wantedString = new Wanted<string>("String");
             Wanted<int> wantedInt = new Wanted<int>(65432);
             Wanted<double> wantedDouble = new Wanted<double>(3.141592);
@@ -20,7 +27,29 @@ namespace CSClass2
             Console.WriteLine(wantedDouble.Value);
 
             Products p = new Products();
-            Console.WriteLine(p[4]);
+            Console.WriteLine(p[4]);*/
+
+
+            Console.Write("숫자 입력 : ");
+            int output;
+            bool result = int.TryParse(Console.ReadLine(), out output);
+
+            if (result)
+            {
+                Console.WriteLine("입력한 숫자 " + output);
+            }
+            else {
+                Console.WriteLine("숫자를 입력해주세요");
+            }
+
+            int x = 0;
+            int y = 0;
+            int vy = 1;
+            int vx = 1;
+
+            Console.WriteLine("현재 좌표 : ( " + x + " , " + y + ") ");
+            NextPos(x, y, vx, vy, out x, out y);
+            Console.WriteLine("다음 좌표 : ( " + x + " , " + y + ") ");
 
         }
     }
