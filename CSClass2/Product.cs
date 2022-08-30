5841301bfaa9817e81d6d7cba7780d6f396ca0f0
soooomm <soooomm00@gmail.com>
2022-08-30 11:28:18 +0900

    #4 구조체 실습

---
+++ CSClass2/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass2
+{
+    class Product : IComparable<Product>
+    {
+        public int Price { get; set; }
+        public string Name { get; set; }
+
+        public int CompareTo(Product other)
+        {
+            return this.Price.CompareTo(other.Price);
+            /*
+            int result = 0;
+
+            if(this.Price < other.Price)result = -1;
+            else if(this.Price > other.Price)result = 1;
+
+            return result;
+            */
+        }
+
+        public override string ToString()
+        {
+            return Name + " : " + Price + "원";
+        }
+    }
+}
--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -8,49 +8,103 @@ namespace CSClass2
 {
     internal class Program
     {
-        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
-        {
-            rx = x + vx;
-            ry = y + vy;
-        }
 
         static void Main(string[] args)
         {
-            /*
             Wanted<string> wantedString = new Wanted<string>("String");
-            Wanted<int> wantedInt = new Wanted<int>(65432);
-            Wanted<double> wantedDouble = new Wanted<double>(3.141592);
-
-
+            Wanted<int> wantedInt = new Wanted<int>(52273);
+            Wanted<double> wantedDouble = new Wanted<double>(52.273);
             Console.WriteLine(wantedString.Value);
             Console.WriteLine(wantedInt.Value);
             Console.WriteLine(wantedDouble.Value);
 
             Products p = new Products();
-            Console.WriteLine(p[4]);*/
+            Console.WriteLine(p[4]);
+            p[4] = 5;
 
-
-            Console.Write("숫자 입력 : ");
+            Console.WriteLine("숫자 입력: ");
             int output;
             bool result = int.TryParse(Console.ReadLine(), out output);
-
             if (result)
             {
-                Console.WriteLine("입력한 숫자 " + output);
+                Console.WriteLine("입력한 숫자: " + output);
             }
-            else {
-                Console.WriteLine("숫자를 입력해주세요");
+            else
+            {
+                Console.WriteLine("숫자를 입력해주세요!");
             }
-
-            int x = 0;
-            int y = 0;
-            int vy = 1;
-            int vx = 1;
-
-            Console.WriteLine("현재 좌표 : ( " + x + " , " + y + ") ");
+            int x = 0; int y = 0;
+            int vx = 1; int vy = 1;
+            Console.WriteLine("현재 좌표: (" + x + "," + y + ")");
             NextPos(x, y, vx, vy, out x, out y);
-            Console.WriteLine("다음 좌표 : ( " + x + " , " + y + ") ");
+            Console.WriteLine("다음 좌표: (" + x + "," + y + ")");
+            Point point;
+            point.x = 10;
+            point.y = 10;
+            point = new Point(3, 5);
+            Console.WriteLine(point.x + " / " + point.y);
 
+            PointClass pointClassA = new PointClass(10, 20);
+            PointClass pointClassB = pointClassA;
+            pointClassB.x = 100; pointClassB.y = 200;
+            Console.WriteLine(pointClassA.x + "/" + pointClassA.y);
+            Console.WriteLine(pointClassB.x + "/" + pointClassB.y);
+
+            PointStruct pointStructA = new PointStruct(10, 20);
+            PointStruct pointStructB = pointStructA;
+            pointStructB.x = 100; pointStructB.y = 200;
+            Console.WriteLine(pointStructA.x + "/" + pointStructA.y);
+            Console.WriteLine(pointStructB.x + "/" + pointStructB.y);
+
+
+
+        }
+
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
+        }
+        struct Point
+        {
+            public int x;
+            public int y;
+            public string testA;
+            public string testB;
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = "초기화";
+                this.testB = "초기화";
+            }
+            public Point(int x, int y, string test)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = test;
+                this.testB = test;
+            }
+        }
+        class PointClass
+        {
+            public int x;
+            public int y;
+            public PointClass(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        struct PointStruct
+        {
+            public int x;
+            public int y;
+            public PointStruct(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
         }
     }
 }
